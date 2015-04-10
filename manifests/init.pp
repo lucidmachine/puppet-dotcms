a847e69fa2552a525934966df597ca004b040730
Alfredo Baldoceda <alfredo@ipsy.com>
2015-04-10 16:53:57 -0700

    Adding postgres variables to init

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,9 +10,13 @@
 class dotcms (
   $package_name = $::dotcms::params::package_name,
   $service_name = $::dotcms::params::service_name,
+  java_home     = $::dotcms::params::java_home,
   $root_plugin  = $::dotcms::params::root_plugin,
   $root_user    = $::dotcms::params::root_user,
   $root_group   = $::dotcms::params::root_group,
+  $postgres_url = $::dotcms::params::postgres_url,
+  $postgres_username = $::dotcms::params::postgres_username,
+  $postgres_password = $::dotcms::params::postgres_password,
 ) inherits ::dotcms::params {
 
   # validate parameters here
