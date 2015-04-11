db69a2f02b8b1fed4c9068d0d618e971a4a1395c
Alfredo Baldoceda <alfredo@ipsy.com>
2015-04-10 17:33:24 -0700

    fixing path for execution, dotcms requires it

--- manifests/plugin/reload.pp
+++ manifests/plugin/reload.pp
@@ -1,22 +1,22 @@
 class dotcms::plugin::reload{
 
+  Exec {
+    path => $::path,
+    cwd  => $::dotcms::server_path,
+    refreshonly => true,
+  }
+
   exec {'Shuting down server':
     command     => "$::dotcms::server_path/bin/shutdown.sh",
-    path        => $::path,
-    refreshonly => true,
   }
 
   exec {'Deploying plugins':
     command     => "$dotcms::server_path/bin/deploy-plugins.sh",
-    path        => $::path,
-    refreshonly => true,
     require     => Exec['Shuting down server']
   }
 
   exec{'Starting up server':
     command     => "$dotcms::server_path/bin/startup.sh",
-    path        => $::path,
-    refreshonly => true,
     require     => Exec['Deploying plugins']
   }
 
