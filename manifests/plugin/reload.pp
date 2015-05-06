e37fb77f72b88ae97bd3bd4585ccfe549de8083f
Alfredo Baldoceda <alfredo@ipsy.com>
2015-05-06 13:25:16 -0700

    fixing typo

--- manifests/plugin/reload.pp
+++ manifests/plugin/reload.pp
@@ -7,21 +7,21 @@ class dotcms::plugin::reload{
   }
 
   exec {'Shuting down server':
-    command => "$::dotcms::server_path/bin/shutdown.sh",
+    command => "${::dotcms::server_path}/bin/shutdown.sh",
   }
 
   exec {'Undeploying plugins':
-    command => "$::dotcms::server_path/bin/undeploy-plugins.sh"
+    command => "${::dotcms::server_path}/bin/undeploy-plugins.sh",
     require => Exec['Shuting down server']
   }
 
   exec {'Deploying plugins':
-    command => "$dotcms::server_path/bin/deploy-plugins.sh",
+    command => "${dotcms::server_path}/bin/deploy-plugins.sh",
     require => Exec['Undeploying plugins']
   }
 
   exec{'Starting up server':
-    command => "$dotcms::server_path/bin/startup.sh",
+    command => "${dotcms::server_path}/bin/startup.sh",
     require => Exec['Deploying plugins']
   }
 
