721e856e13421730aa53d76663309c250b045209
Alfredo Baldoceda <alfredo@ipsy.com>
2015-05-06 11:26:48 -0700

    adding undeploying plugins

--- manifests/plugin/reload.pp
+++ manifests/plugin/reload.pp
@@ -7,17 +7,22 @@ class dotcms::plugin::reload{
   }
 
   exec {'Shuting down server':
-    command     => "$::dotcms::server_path/bin/shutdown.sh",
+    command => "$::dotcms::server_path/bin/shutdown.sh",
+  }
+
+  exec {'Undeploying plugins':
+    command => "$::dotcms::server_path/bin/undeploy-plugins.sh"
+    require => Exec['Shuting down server']
   }
 
   exec {'Deploying plugins':
-    command     => "$dotcms::server_path/bin/deploy-plugins.sh",
-    require     => Exec['Shuting down server']
+    command => "$dotcms::server_path/bin/deploy-plugins.sh",
+    require => Exec['Undeploying plugins']
   }
 
   exec{'Starting up server':
-    command     => "$dotcms::server_path/bin/startup.sh",
-    require     => Exec['Deploying plugins']
+    command => "$dotcms::server_path/bin/startup.sh",
+    require => Exec['Deploying plugins']
   }
 
 }
