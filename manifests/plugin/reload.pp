5812bdd7e94dfd7ecd6f392a4637d009b0321aef
Eric Slenk <slenkeri@anr.msu.edu>
2017-04-17 18:57:08 -0400

    Fix more paths.

--- manifests/plugin/reload.pp
+++ manifests/plugin/reload.pp
@@ -6,24 +6,24 @@ class dotcms::plugin::reload{
     refreshonly => true,
   }
 
-  exec {'Shutting down server':
-    command => "${::dotcms::dotcms_path}/bin/shutdown.sh",
+  exec { 'Shutting down server':
+    command => "${::dotcms::dotcms_distro_path}/bin/shutdown.sh",
     returns => [0, 1]
 
   }
 
-  exec {'Undeploying plugins':
-    command => "${::dotcms::dotcms_path}/bin/undeploy-plugins.sh",
+  exec { 'Undeploying plugins':
+    command => "${::dotcms::dotcms_distro_path}/bin/undeploy-plugins.sh",
     require => Exec['Shutting down server']
   }
 
-  exec {'Deploying plugins':
-    command => "${dotcms::dotcms_path}/bin/deploy-plugins.sh",
+  exec { 'Deploying plugins':
+    command => "${dotcms::dotcms_distro_path}/bin/deploy-plugins.sh",
     require => Exec['Undeploying plugins']
   }
 
-  exec{'Starting up server':
-    command => "${dotcms::dotcms_path}/bin/startup.sh",
+  exec{ 'Starting up server':
+    command => "${dotcms::dotcms_distro_path}/bin/startup.sh",
     require => Exec['Deploying plugins']
   }
 
